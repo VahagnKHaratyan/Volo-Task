2f76b088ca7528422a54cf2674f45bfa6dba98b3
VahagnKHaratyan <kharatyan.vahagn@gmail.com>
2016-09-09 16:09:28 +0400

    Add entity Model and Books Controller

---
+++ BookStore/BookStore/Controllers/BooksController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BookStore.Entity;
+
+namespace BookStore.Controllers
+{
+    public class BooksController : Controller
+    {
+        private DatabaseEntities db = new DatabaseEntities();
+
+        // GET: Books
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Books.ToListAsync());
+        }
+
+        // GET: Books/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Books books = await db.Books.FindAsync(id);
+            if (books == null)
+            {
+                return HttpNotFound();
+            }
+            return View(books);
+        }
+
+        // GET: Books/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Books/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "id,title,price,page_count,book_description,picture")] Books books)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Books.Add(books);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(books);
+        }
+
+        // GET: Books/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Books books = await db.Books.FindAsync(id);
+            if (books == null)
+            {
+                return HttpNotFound();
+            }
+            return View(books);
+        }
+
+        // POST: Books/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "id,title,price,page_count,book_description,picture")] Books books)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(books).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(books);
+        }
+
+        // GET: Books/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Books books = await db.Books.FindAsync(id);
+            if (books == null)
+            {
+                return HttpNotFound();
+            }
+            return View(books);
+        }
+
+        // POST: Books/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Books books = await db.Books.FindAsync(id);
+            db.Books.Remove(books);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
